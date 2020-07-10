31239d0237034ce02443a30effaaa58153703eb2
Tyler Goodwin <tyler.goodwin23@gmail.com>
2020-07-10 19:47:52 +1000

    update frontend api client

--- frontend/src/lib/ApiClient.js
+++ frontend/src/lib/ApiClient.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+function buildPayload(orderItems) {
+  return {
+    order: {
+      order_items_attributes: orderItems
+    }
+  }
+}
+
 export default class ApiClient {
   constructor() {
     this.client = axios.create({
@@ -7,15 +15,33 @@ export default class ApiClient {
     })
   }
 
-  async getAvailableProducts() {
+  async get(path) {
     try {
-      const res = await this.client.get("/products")
+      const res = await this.client.get(path)
       if (res.status !== 200) {
         throw new Error(`Unexpected HTTP Status: ${res.status}`);
       }
       return res.data;
     } catch (err) {
-      throw new Error(`Could not retrieve products: ${err}`);
+      throw new Error(`Could not retrieve ${path}: ${err}`);
     }
+  }
+
+  async getAvailableProducts() {
+    return await this.get("products")
+  }
+
+  async getOrders() {
+    return await this.get("/orders")
+  }
+
+  async getOrderPrices(orderItems) {
+    const payload = buildPayload(orderItems);
+    return await this.client.post("/orders", JSON.stringify(payload));
+  }
+
+  async createOrder(orderItems) {
+    const payload = buildPayload(orderItems);
+    return await this.client.post("/orders", JSON.stringify(payload));
   }
 }
